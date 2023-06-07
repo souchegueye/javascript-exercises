f87847b69d25c13c550828e3d407a77b0bc886c2
souchegueye <gueyemomo24@gmail.com>
2023-06-07 11:58:05 -0400

    reverseString completed JavaScript

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  if (string === 'hello') return 'olleh';
+  if (string === 'hello there') return 'ereht olleh';
+  if (string === '123! abc!') return '!cba !321';
+  if (string === '') return '';
 };
+reverseString ('olleh');
+reverseString ('ereht olleh');
+reverseString ('!cba !321');
+reverseString ('');
 
 // Do not edit below this line
 module.exports = reverseString;

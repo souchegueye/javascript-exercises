ad8a01b6b0a53f81ba00acc5be4e3219e9d6a012
souchegueye <gueyemomo24@gmail.com>
2023-06-07 13:22:22 -0400

    getTheTitles completed JavaScript

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+  //we used the map to be able to do that
+  return array.map (book => book.title);
 };
 
 // Do not edit below this line

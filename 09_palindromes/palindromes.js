15065a1379b70fe0034535eafbb8f09a98545261
souchegueye <gueyemomo24@gmail.com>
2023-06-07 13:08:39 -0400

    palindromes completed JavaScript

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  var re = /[^A-Za-z0-9]/g;
+  string = string.toLowerCase ().replace (re, '');
+  var len = string.length;
+  for (var i = 0; i < len / 2; i++) {
+    if (string[i] !== string[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
-
+palindromes ('');
 // Do not edit below this line
 module.exports = palindromes;

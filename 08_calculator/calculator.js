3da5d732a499bf21b71bf39ccbffb47a9a5ab6e8
souchegueye <gueyemomo24@gmail.com>
2023-06-07 12:55:53 -0400

    calculator completed JavaScript

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce ((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.length
+    ? array.reduce ((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return Math.pow (num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };

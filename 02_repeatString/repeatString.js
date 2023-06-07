500a1d6046495c9424e1439213cbe8c628c2b8bc
souchegueye <gueyemomo24@gmail.com>
2023-06-07 11:26:17 -0400

    repeat String javascript

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) return 'ERROR';
+  if (num === 0) return '';
+  if (num === 1) return string;
+  else return string + repeatString (string, num - 1);
 };
+repeatString ('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
